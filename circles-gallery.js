0bf60c27adc0d9337bd3352e5d300aa26fd43325
Ahmed Hesham <ahesham@issi.net>
2023-01-12 23:52:17 +0200

    added animate function

--- circles-gallery.js
+++ circles-gallery.js
@@ -23,9 +23,14 @@ function init(element, options) {
     }
     element.appendChild(galleryItems);
 
+    
     var activeItem = document.querySelector('.active-item');
+    //animate(activeItem, 5000, 0, 5);
+
+    // setting style for each gallery item
     var galleryItemsAll = document.querySelectorAll('.gallery-item');
     galleryItemsAll.forEach(function(item, i) {
+        // set position with reference to active gallery item
         item.style.top = angleToX(
             activeItem.offsetTop - item.clientHeight / 2, 
             activeItem.clientHeight / 2 + item.clientHeight / 2 + options.distance, 
@@ -35,19 +40,45 @@ function init(element, options) {
             activeItem.clientWidth / 2 + item.clientWidth / 2 + options.distance, 
             - (270 / galleryItemsAll.length * (i + 1) - 45)) + 'px';
         item.style.background = '#de7f5f';
+        //animate(item, 4000, 360 / galleryItemsAll.length * i, 50);
     });
 }
 
+// Got the idea from here https://stackoverflow.com/a/43642478/2398288
+// calculate x coordinate from angle, distance, and an origin point
+// originX is the x coordinate of origin point
+// r is radius (distance)
+// theta is angle
 function angleToX(originX, r, theta) {
     theta = (theta - 180) * Math.PI / 180;
 
     return originX + r * Math.cos(theta);
 }
 
+// Got the idea from here https://stackoverflow.com/a/43642478/2398288
+// calculate y coordinate from angle, distance, and an origin point
+// originY is the y coordinate of origin point
+// r is radius (distance)
+// theta is angle
 function angleToY(originY, r, theta) {
     theta = (theta - 180) * Math.PI / 180;
 
     return originY - r * Math.sin(theta);
+}
+
+function animate(element, duration, startAngle, r){
+    var steps = 0.0;
+    setInterval(() => {
+        if(steps == duration / 10) {
+            steps = 0.0;
+        }
+
+        element.style.transform = `rotate(${startAngle + (360 / (duration / 10) * steps)}deg)`;
+        element.style.transform += `translate(${r}px)`; 
+        element.style.transform += `rotate(${- (startAngle + (360 / (duration / 10) * steps))}deg)`;
+
+        steps++;
+    }, 10);
 }
 
 // add circle gallery function to DOM elements when using Vanilla JavaScript
